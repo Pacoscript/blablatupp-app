6b18d1d83322bf08f975b151400a4d89b5ad5ac2
Paco <franfercant@gmail.com>
2020-07-22 16:52:48 +0200

    login page created

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <form class="login-form">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Username</label>
+      <input
+        type="email"
+        class="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+      />
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" />
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  components: {},
+}
+</script>
