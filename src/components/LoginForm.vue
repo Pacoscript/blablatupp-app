474a255e404ad7046c663b332f082134fcc08972
Paco <franfercant@gmail.com>
2020-08-10 16:44:11 +0200

    added login console log logic

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form class="login-form">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Username</label>
+      <input
+        class="form-control"
+        id="userNameInput"
+        aria-describedby="emailHelp"
+        v-model="username"
+      />
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input
+        type="password"
+        class="form-control"
+        id="passwordInput"
+        v-model="password"
+      />
+    </div>
+    <button v-on:click="submitLogin" type="submit" class="btn btn-primary">
+      Submit
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data: () => {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitLogin: function() {
+      console.log(this.username, this.password)
+    },
+  },
+}
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,17 @@
 <template>
-  <form class="login-form">
-    <div class="form-group">
-      <label for="exampleInputEmail1">Username</label>
-      <input
-        type="email"
-        class="form-control"
-        id="exampleInputEmail1"
-        aria-describedby="emailHelp"
-      />
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" />
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+  <div class="login">
+    <LoginForm />
+  </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import LoginForm from '@/components/LoginForm.vue'
+
 export default {
   name: 'Login',
-  components: {},
+  components: {
+    LoginForm,
+  },
 }
 </script>
